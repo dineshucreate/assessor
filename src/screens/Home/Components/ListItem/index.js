1ca47a4a8d0b2b93ed91f9547e38cb7a24ef8e5c
Gurpreet Singh <gurpreetsingh@ucreate.co.in>
2019-06-11 17:55:52 +0530

    Fixed lint errors and destructoring issues

--- src/screens/Home/Components/ListItem/index.js
+++ src/screens/Home/Components/ListItem/index.js
@@ -1,33 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Text, View, Image } from 'react-native';
 import styles from './style';
 
-const renderTechnology = ({technologies}) => {
-    return technologies.map((item, index) => (
-        <Image 
-            key={index} 
-            style={styles.styleImageTech}
-            source={{ uri: item }}
-       />
-        )
-    )
-}
+const renderTechnology = (technologies) => {
+  return technologies.map((item, index) => (
+    <Image
+      key={index}
+      style={styles.styleImageTech}
+      source={{ uri: item }}
+    />
+  )
+  );
+};
 
-const ListItem = (props) => (
-        <View style={styles.styleMainContainer}>
-            <Image
-                style={styles.styleImage}
-                resizeMode='stretch'
-                source={{ uri: props.dataItem.pic }}
-            />
-            <View style={styles.styleInfoContainer}>
-                <Text style={styles.styleTitle}>{props.dataItem.name}</Text>
-                <View style={styles.styleTechnologies} >
-                    { renderTechnology(props.dataItem) }
-                </View>
-                <Text style={styles.styleExperienceText}>Total Experience: {props.dataItem.totalExperience}</Text>
-            </View>
-        </View>
-    );
+const ListItem = (props) => {
+  const { dataItem: { name, totalExperience, pic, technologies } } = props;
+  return (<View style={styles.styleMainContainer}>
+    <Image
+      style={styles.styleImage}
+      resizeMode="stretch"
+      source={{ uri: pic }}
+    />
+    <View style={styles.styleInfoContainer}>
+      <Text style={styles.styleTitle}>{name}</Text>
+      <View style={styles.styleTechnologies} >
+        { renderTechnology(technologies) }
+      </View>
+      <Text style={styles.styleExperienceText}>Total Experience: {totalExperience}</Text>
+    </View>
+  </View>);
+};
+
+ListItem.propTypes = {
+  dataItem: PropTypes.object,
+};
 
 export default ListItem;
