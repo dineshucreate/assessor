cc756a98dc528277ca944c7cdefefccd70b1834f
kulbhushan-ucreate <Mac3@Mac-Mini-3.local>
2019-06-12 13:36:08 +0530

    rename height and width key name

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,8 +1,6 @@
 import { Dimensions } from 'react-native';
-const { height, width } = Dimensions.get('window');
 
-const deviceHeight = height;
-const deviceWidth = width;
+const { height: deviceHeight, width: deviceWidth } = Dimensions.get('window');
 
 export default {
   deviceHeight,
