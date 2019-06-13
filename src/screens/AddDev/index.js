01ae91c2eb18f5c0a0cdec85175348b8cb91dd7a
Gurpreet Singh <gurpreetsingh@ucreate.co.in>
2019-06-13 17:14:49 +0530

    Implemented add dev functionality

--- src/screens/AddDev/index.js
+++ src/screens/AddDev/index.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
-import { View, Image, Text, TextInput, FlatList } from 'react-native';
+import { View, Image, Text, TextInput, FlatList, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import firebase from 'react-native-firebase';
 import { styles } from './styles';
 import { devList } from '../Home/devList';
 import AppHeader from '../../components/AppHeader';
 import ListItem from './Components/ListItem';
 import navigationService from '../../utilities/navigationService';
 
-export default class AddDev extends Component {
+class AddDev extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      technologies: [
-        'iOS',
-        'Android',
-        'React Native',
-      ],
+      name: 'Gurpreet Singh',
+      email: 'gurpreetsingh@ucreate.co.in',
+      technologies: ['iOS', 'Android', 'React Native'],
+      experience: [0, 0, 0],
     };
   }
 
   saveAndGoBack = () => {
-    navigationService.goBack();
+    const { loader } = this.props;
+    loader.open();
+    const { name, email, technologies, experience } = this.state;
+    const tech = technologies.map((item, index) => {
+      return { tech: item, exp: experience[index] };
+    });
+    const techs = tech.filter((item) => item.exp > 0);
+    const ref = firebase.firestore().collection('Developers').doc(email);
+
+    firebase.firestore().runTransaction(async (transaction) => {
+      const doc = await transaction.get(ref);
+      // if it does not exist set the population to one
+      if (!doc.exists) {
+        transaction.set(ref, { name, email, technologies: techs });
+        // return the new value so we know what the new population is
+        return { name, email, technologies: techs };
+      }
+      transaction.update(ref, { name, email, technologies: techs });
+      // return the new value so we know what the new population is
+      return { name, email, technologies: techs };
+    })
+      .then(() => {
+        navigationService.goBack();
+        loader.close();
+      })
+      .catch(() => {
+        Alert.alert('Save developer faild');
+        loader.close();
+      });
+  };
+
+  addExperience = (index, value, isChecked) => {
+    const { experience } = this.state;
+    const expArray = [...experience];
+    expArray[index] = isChecked ? value : 0;
+    this.setState({ experience: expArray });
   };
 
   goBack = () => {
@@ -29,12 +65,12 @@ export default class AddDev extends Component {
 
   renderItem = ({ item, index }) => {
     return (<View >
-      <ListItem key={index} dataItem={item} isGrid={this.state.isSwitchOn} />
+      <ListItem key={index} dataItem={item} add={(value, isChecked) => this.addExperience(index, value, isChecked)} />
     </View>);
   };
 
   render() {
-    const { name, technologies } = this.state;
+    const { name, email, technologies } = this.state;
     return (
       <View style={styles.container}>
         <AppHeader
@@ -63,6 +99,19 @@ export default class AddDev extends Component {
               clearButtonMode="always"
             />
           </View>
+          <View style={styles.nameContainer}>
+            <Text style={styles.devNameText}>
+                Email
+            </Text>
+            <TextInput
+              style={styles.devNameTextInput}
+              autoCapitalize="none"
+              placeholder="Enter Email"
+              onChangeText={(text) => this.setState({ email: text })}
+              value={email}
+              clearButtonMode="always"
+            />
+          </View>
           <Text style={styles.styleInsturctions}>
                 Select technology and specify experience in years
           </Text>
@@ -77,3 +126,14 @@ export default class AddDev extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  loader: state.welcomeReducer.loader,
+});
+
+AddDev.propTypes = {
+  loader: PropTypes.object,
+};
+
+export default connect(mapStateToProps)(AddDev);
+
